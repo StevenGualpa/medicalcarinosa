dd13cdd1ce7a499a0d07ad6f3035567e7c3b41d3
Steven Gualpa <68717276+StevenGualpa@users.noreply.github.com>
2024-01-17 20:39:57 -0500

    inicio borrado .idea

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"GolandProyectos/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/viper"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"log"
 )
 
@@ -30,6 +33,15 @@ func main() {
 	}
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
+	})
+
+	dsn := "host=ep-lingering-snowflake-a5j9m53w.us-east-2.aws.neon.tech user=stevengualpa password=VamLyM2btnd4 dbname=carinosabd port=5432 sslmode=verify-full"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	db.AutoMigrate(&models.User{})
+	db.Create(&models.User{
+		FirstName: "Steven",
+		LastName:  "Gualpa",
 	})
 
 	app.Listen(":" + config.GetString("APP_PORT"))
