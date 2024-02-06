48fbcaeaada549c7a3bd0a1d4724d156856b25bb
Steven Gualpa <68717276+StevenGualpa@users.noreply.github.com>
2024-02-05 19:19:05 -0500

    Primera version de User

--- routers/router.go
+++ routers/router.go
@@ -8,9 +8,9 @@ import (
 
 // SetupUserRoutes configura las rutas relacionadas con los usuarios.
 func SetupUserRoutes(app *fiber.App, userHandler handlers.UserHandler) {
-	app.Get("/api/users", userHandler.GetUsers)
+	app.Get("/Users/GetAll", userHandler.GetUsers)
 	app.Get("/api/users/:id", userHandler.GetUser)
-	app.Post("/api/users", userHandler.CreateUser)
+	app.Post("/Users/Insert", userHandler.CreateUser)
 	app.Put("/api/users/:id", userHandler.UpdateUser)
 	app.Delete("/api/users/:id", userHandler.DeleteUser)
 }
