88295e5fed8d85734702ccbec833bde65b93e933
Steven Gualpa <68717276+StevenGualpa@users.noreply.github.com>
2024-02-16 21:51:21 -0500

    Primera version de Pacientecuiadorr

--- repository/PacienteCuidadorRepository.go
+++ repository/PacienteCuidadorRepository.go
@@ -2,6 +2,8 @@ package repository
 
 import (
 	"GolandProyectos/models"
+	"errors"
+	"fmt"
 	"gorm.io/gorm"
 )
 
@@ -25,6 +27,27 @@ func NewPacienteCuidadorRepository(db *gorm.DB) PacienteCuidadorRepository {
 
 // Create inserta una nueva relación PacienteCuidador en la base de datos.
 func (r *pacienteCuidadorRepository) Create(pc models.PacienteCuidador) (models.PacienteCuidador, error) {
+	// Verifica si existe el Paciente
+	var pacienteExistente models.Paciente
+	if err := r.db.First(&pacienteExistente, pc.PacienteID).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return models.PacienteCuidador{}, fmt.Errorf("el paciente con ID %d no existe", pc.PacienteID)
+		}
+		// Manejar otros posibles errores de la base de datos
+		return models.PacienteCuidador{}, err
+	}
+
+	// Verifica si existe el Cuidador
+	var cuidadorExistente models.Cuidador
+	if err := r.db.First(&cuidadorExistente, pc.CuidadorID).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return models.PacienteCuidador{}, fmt.Errorf("el cuidador con ID %d no existe", pc.CuidadorID)
+		}
+		// Manejar otros posibles errores de la base de datos
+		return models.PacienteCuidador{}, err
+	}
+
+	// Si ambos existen, procede a crear la relación PacienteCuidador
 	result := r.db.Create(&pc)
 	if result.Error != nil {
 		return models.PacienteCuidador{}, result.Error
