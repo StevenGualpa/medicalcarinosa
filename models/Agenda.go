f5d4d305239938999501f0722eff847afe602312
Steven Gualpa <68717276+StevenGualpa@users.noreply.github.com>
2024-02-16 22:52:17 -0500

    Primera version de Pacientecuiadorr

--- models/Agenda.go
+++ models/Agenda.go
@@ -8,9 +8,9 @@ import (
 // Agenda representa la programación de citas o eventos.
 type Agenda struct {
 	gorm.Model
-	PacienteCuidadorID uint             `gorm:"not null"`                              // Clave foránea de la relación PacienteCuidador.
-	Fecha              time.Time        `gorm:"type:date"`                             // La fecha del evento.
-	Hora               string           `gorm:"type:time"`                             // La hora del evento.
-	Estado             string           `gorm:"type:varchar(100);default:'pendiente'"` // Estado del evento (pendiente, cancelado).
-	PacienteCuidador   PacienteCuidador `gorm:"foreignKey:PacienteCuidadorID"`         // Relación con PacienteCuidador.
+	PacienteCuidadorID uint             `gorm:"not null"`
+	Fecha              time.Time        `gorm:"type:date"`
+	Hora               string           `gorm:"type:time"` // Asegúrate de que este tipo sea compatible con tu SGBD.
+	Estado             string           `gorm:"type:varchar(100);default:'pendiente'"`
+	PacienteCuidador   PacienteCuidador `gorm:"foreignKey:PacienteCuidadorID"`
 }
