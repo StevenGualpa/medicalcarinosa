0db667bec3e47b53802e8db82f9ae3dc313c5463
Steven Gualpa <68717276+StevenGualpa@users.noreply.github.com>
2024-02-21 21:40:36 -0500

    Primera version de Pacientecuiadorr

--- models/Agenda.go
+++ models/Agenda.go
@@ -10,7 +10,9 @@ type Agenda struct {
 	gorm.Model
 	PacienteCuidadorID uint             `gorm:"not null"`
 	Fecha              time.Time        `gorm:"type:date"`
-	Hora               time.Time        `gorm:"type:timestamptz"` // Cambiado a time.Time
+	Hora               time.Time        `gorm:"type:timestamptz"` // Asegúrate que este tipo sea compatible con tu base de datos
 	Estado             string           `gorm:"type:varchar(100);default:'pendiente'"`
+	Nombre             string           `gorm:"type:varchar(255)"` // Agregado
+	Descripcion        string           `gorm:"type:text"`         // Agregado
 	PacienteCuidador   PacienteCuidador `gorm:"foreignKey:PacienteCuidadorID"`
 }
