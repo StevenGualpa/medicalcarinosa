7cf6cf456dfc0fb7bcec9906793ea00c5b51580c
Steven Gualpa <68717276+StevenGualpa@users.noreply.github.com>
2024-02-21 22:15:06 -0500

    Primera version de Pacientecuiadorr

--- models/Agenda.go
+++ models/Agenda.go
@@ -16,3 +16,19 @@ type Agenda struct {
 	Descripcion        string           `gorm:"type:text"`         // Agregado
 	PacienteCuidador   PacienteCuidador `gorm:"foreignKey:PacienteCuidadorID"`
 }
+
+// Define un modelo de detalle para capturar los resultados de tu consulta
+type AgendaDetalle struct {
+	ID               uint      `json:"id"`
+	Nombre           string    `json:"nombre"`
+	Descripcion      string    `json:"descripcion"`
+	Fecha            time.Time `json:"fecha"`
+	Hora             time.Time `json:"hora"`
+	Estado           string    `json:"estado"`
+	PacienteID       uint      `json:"paciente_id"`
+	PacienteNombre   string    `json:"paciente_nombre"`
+	PacienteApellido string    `json:"paciente_apellido"`
+	CuidadorID       uint      `json:"cuidador_id"`
+	CuidadorNombre   string    `json:"cuidador_nombre"`
+	CuidadorApellido string    `json:"cuidador_apellido"`
+}
--- repository/AgendaRepository.go
+++ repository/AgendaRepository.go
@@ -9,7 +9,7 @@ type AgendaRepository interface {
 	Create(agenda models.Agenda) (models.Agenda, error)
 	Update(agenda models.Agenda) (models.Agenda, error)
 	Delete(id uint) error
-	GetAll() ([]models.Agenda, error)
+	GetAll() ([]models.AgendaDetalle, error)
 	GetById(id uint) (models.Agenda, error)
 }
 
@@ -39,12 +39,28 @@ func (r *agendaRepository) Delete(id uint) error {
 	return r.db.Delete(&models.Agenda{}, id).Error
 }
 
-func (r *agendaRepository) GetAll() ([]models.Agenda, error) {
-	var agendas []models.Agenda
-	if err := r.db.Preload("PacienteCuidador").Preload("PacienteCuidador.Paciente").Preload("PacienteCuidador.Cuidador").Preload("PacienteCuidador.Paciente.User").Preload("PacienteCuidador.Cuidador.User").Find(&agendas).Error; err != nil {
+func (r *agendaRepository) GetAll() ([]models.AgendaDetalle, error) {
+	var detalles []models.AgendaDetalle
+
+	// Tu consulta SQL personalizada
+	query := `
+    SELECT ag.id, ag.nombre, ag.descripcion, ag.fecha, ag.hora, ag.estado,
+           pa.id as paciente_id, us1.first_name as paciente_nombre, us1.last_name as paciente_apellido,
+           cu.id as cuidador_id, us2.first_name as cuidador_nombre, us2.last_name as cuidador_apellido
+    FROM agendas as ag
+    JOIN paciente_cuidadors as pc ON ag.paciente_cuidador_id = pc.id
+    JOIN pacientes as pa ON pc.paciente_id = pa.id
+    JOIN cuidadors as cu ON pc.cuidador_id = cu.id
+    JOIN users as us1 ON pa.user_id = us1.id
+    JOIN users as us2 ON cu.user_id = us2.id
+    `
+
+	// Ejecutar la consulta
+	if err := r.db.Raw(query).Scan(&detalles).Error; err != nil {
 		return nil, err
 	}
-	return agendas, nil
+
+	return detalles, nil
 }
 
 func (r *agendaRepository) GetById(id uint) (models.Agenda, error) {
