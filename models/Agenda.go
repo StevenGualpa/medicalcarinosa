39c3a001fde12929a2924b5ce81a42b8b39f36dd
Steven Gualpa <68717276+StevenGualpa@users.noreply.github.com>
2024-02-24 21:06:42 -0500

    Primera version de Pacientecuiadorr

--- models/Agenda.go
+++ models/Agenda.go
@@ -8,13 +8,13 @@ import (
 // Agenda representa la programación de citas o eventos.
 type Agenda struct {
 	gorm.Model
-	PacienteCuidadorID uint             `gorm:"not null"`
-	Fecha              time.Time        `gorm:"type:date"`
-	Hora               time.Time        `gorm:"type:timestamptz"` // Asegúrate que este tipo sea compatible con tu base de datos
-	Estado             string           `gorm:"type:varchar(100);default:'pendiente'"`
-	Nombre             string           `gorm:"type:varchar(255)"` // Agregado
-	Descripcion        string           `gorm:"type:text"`         // Agregado
-	PacienteCuidador   PacienteCuidador `gorm:"foreignKey:PacienteCuidadorID"`
+	PacienteID  uint      `gorm:"not null"` // Ahora apunta directamente al ID de paciente
+	Fecha       time.Time `gorm:"type:date"`
+	Hora        time.Time `gorm:"type:timestamptz"` // Asegúrate que este tipo sea compatible con tu base de datos
+	Estado      string    `gorm:"type:varchar(100);default:'pendiente'"`
+	Nombre      string    `gorm:"type:varchar(255)"`     // Agregado
+	Descripcion string    `gorm:"type:text"`             // Agregado
+	Paciente    Paciente  `gorm:"foreignKey:PacienteID"` // Actualizado para referenciar la clave foránea correcta
 }
 
 // Define un modelo de detalle para capturar los resultados de tu consulta
